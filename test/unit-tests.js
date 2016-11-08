fc16fb5bf2dfab5bf59c3afeecbdf597ef965c49
lrknaff <lacey.knaff@gmail.com>
2016-11-08 14:28:50 -0700

    one passing test

---
+++ test/unit-tests.js
@@ -0,0 +1,8 @@
+import { assert } from 'chai';
+import scoreWord from '../lib/scoreWord.js';
+
+describe('scoreWord', () => {
+  it('should be a function', () => {
+    assert.isFunction(scoreWord);
+  });
+});
