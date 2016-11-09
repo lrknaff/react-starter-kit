92ba9d13f9a54918814e8d03db95410d4020c3d3
lrknaff <lacey.knaff@gmail.com>
2016-11-08 17:27:32 -0700

    initial setup complete

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -3,4 +3,11 @@ import { scoreWord } from '../scoreWord'
 
 export default class Application extends Component {
 
+  render() {
+    return (
+      <div className="main-container">
+        <h2>Hello!</h2>
+      </div>
+    )
+  }
 }
