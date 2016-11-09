00cfe0b9943a8b69821effe153e7da31e69bee15
lrknaff <lacey.knaff@gmail.com>
2016-11-08 18:12:40 -0700

    working tests

--- test/unit-tests.js
+++ test/unit-tests.js
@@ -5,16 +5,40 @@ describe('scoreWord', () => {
   it('should be a function', () => {
     assert.isFunction(scoreWord)
   })
-  it.skip('should take a word', () => {
 
+  context('word score', () => {
+    it('should return a value of 8 if the word is hello', () => {
+      const score = scoreWord('hello')
+      assert.equal(score, 8)
+    })
+    it('should return a value of 9 if the word is world', () => {
+      const score = scoreWord('world')
+      assert.equal(score, 9)
+    })
+    it('should return a value of 0 if the world is null', () => {
+      const score = scoreWord(null)
+      assert.equal(score, 0)
+    })
   })
-  it.skip('should take a multiplier', () => {
 
+  context('multiplier', () => {
+    it.skip('should turn all letters to uppercase', () => {
+
+    })
   })
-  it.skip('should return a value of 8 if the word is hello', () => {
 
-  })
-  it.skip('should turn all letters to uppercase', () => {
-
+  context('more functionality', () => {
+    it('should not be case sensitive', () => {
+      const score = scoreWord('HELLO')
+      assert.equal(score, 8)
+    })
+    it('should not be case sensitive', () => {
+      const score = scoreWord('wOrLd')
+      assert.equal(score, 9)
+    })
+    it('should not count white space', () => {
+      const score = scoreWord('   hello')
+      assert.equal(score, 8)
+    })
   })
 })
